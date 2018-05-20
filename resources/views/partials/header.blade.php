c997bbb0e5a3fe2af46ba674eb657c7e152976db
Yoga <yoga.h@smooets.com>
2018-05-20 17:11:12 +0700

    Remove Menu Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -64,16 +64,13 @@
                                     <div class="logged-user-avatar-info">
                                         <div class="avatar-w"><img alt="" src="{{asset('themes/img/avatar1.jpg')}}"></div>
                                         <div class="logged-user-info-w">
-                                            <div class="logged-user-name">Maria Gomez</div>
+                                            <div class="logged-user-name">{{ Sentinel::getUser()->first_name }}</div>
                                             <div class="logged-user-role">Administrator</div>
                                         </div>
                                     </div>
                                     <div class="bg-icon"><i class="os-icon os-icon-wallet-loaded"></i></div>
                                     <ul>
-                                        <li><a href="apps_email.html"><i class="os-icon os-icon-mail-01"></i><span>Incoming Mail</span></a></li>
                                         <li><a href="users_profile_big.html"><i class="os-icon os-icon-user-male-circle2"></i><span>Profile Details</span></a></li>
-                                        <li><a href="users_profile_small.html"><i class="os-icon os-icon-coins-4"></i><span>Billing Details</span></a></li>
-                                        <li><a href="#"><i class="os-icon os-icon-others-43"></i><span>Notifications</span></a></li>
                                         <li><a href="{{ route('auth.logout') }}"><i class="os-icon os-icon-signs-11"></i><span>Logout</span></a></li>
                                     </ul>
                                 </div>
