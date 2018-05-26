bc3177fe5223c20335a83285bff77d13fae2e47a
Yoga <yoga.h@smooets.com>
2018-05-26 21:32:16 +0700

    Case Sensitive

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::group([ 'middleware' => 'sentinel_auth' ], function () {
         Route::patch('transaction/{id}/approve', array('as' => 'transaction.approve', 'uses' => 'TransactionController@approve'));
         Route::patch('transaction/{id}/reject', array('as' => 'transaction.reject', 'uses' => 'TransactionController@reject'));
         Route::resource('voucher', 'VoucherController');
-        Route::resource('lg', 'LgController');
+        Route::resource('lg', 'LGController');
         Route::resource('delivery', 'DeliveryController');
 
     });
