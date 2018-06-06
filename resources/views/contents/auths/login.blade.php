281ce72bd269151369e86037d7164313d5b85669
Fadli Muhamad Faridi <fadlimuhamadfaridi@Fadlis-MacBook-Pro.local>
2018-06-06 12:30:30 +0700

    remove word login form at form login

--- resources/views/contents/auths/login.blade.php
+++ resources/views/contents/auths/login.blade.php
@@ -6,7 +6,7 @@
     <div class="logo-w">
         <a href="{{route('auth.login')}}"><img class="img-responsive" style="width:100%;" src="{{asset('themes/img/logo-big.png')}}"></a>
     </div>
-    <h4 class="auth-header">Login Form</h4>
+    
     <div style="width:85%;margin:0 auto;">
         @include('flash::message')
     </div>
