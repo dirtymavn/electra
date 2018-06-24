cf99f9de72b6ec2b914a6bd472f1da241f0a254b
Yoga <yoga.h@smooets.com>
2018-06-24 16:31:29 +0700

    Fixing Voucher

--- resources/views/contents/business/voucher/index.blade.php
+++ resources/views/contents/business/voucher/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Voucher')
+@section('title', 'Business - Voucher')
 
 @section('style')
 <link rel="stylesheet" type="text/css" href="{{asset('css/switch-custom.css')}}">
@@ -9,7 +9,7 @@
 @section('breadcrumb')
 <ul class="breadcrumb">
     <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
-    <li class="breadcrumb-item">Voucher</li>
+    <li class="breadcrumb-item"><a href="{{route('voucher.index')}}">Voucher</a></li>
 </ul>
 @endsection
 
@@ -20,6 +20,10 @@
 <a href="{{ route('voucher.create')}}" class="btn btn-primary" id="btn-submit">
     <i class="fa fa-plus m-right-10"></i> Add Voucher
 </a>
+<button class="btn btn-danger" id="bulk-delete">
+    <i class="fa fa-trash m-right-10"></i> Bulk Delete
+</button>
+<br>
 <br>
 
 <div class="table-responsive">
@@ -30,4 +34,71 @@
 @endsection
 @section('script')
 {!! $dataTable->scripts() !!}
+
+<script>
+    $(document).ready(function() {
+        spinnerLoad($('#form-voucher'));
+    });
+
+    $(document).on('click', '#bulk-delete', function() {
+        var lengthChecked = $('td .checklist:checked').length;
+        var ids = [];
+        if (lengthChecked <= 0) {
+            alert('Please checklist one or more data');
+            return false;
+        }
+        
+        $('td .checklist:checked').each(function(i, obj) {
+            var href = $('td.row-actions').eq(i).find('.deleteData').data('href');
+            var id = href.split('/')[5];
+            ids.push(id);
+        });
+        
+        if (ids.length > 0) {
+            bulkDelete("{{ route('voucher.bulk-delete') }}", ids);
+            // $.ajax({
+            //     url: "{{ route('voucher.bulk-delete') }}",
+            //     method: "POST",
+            //     dataType: "json",
+            //     data: {
+            //         'ids': ids
+            //     },
+            //     beforeSend: function(data) {
+            //         $('div.spinner').show();
+            //     },
+            //     success: function(data) {
+            //         $('div.spinner').hide();
+            //         $('.datatable').DataTable().ajax.reload();
+            //         $('th .checklist').prop('checked', false);
+            //     },
+            //     error: function(data) {
+            //         alert('Something went wrong!');
+            //         location.reload();
+            //     }
+            // });
+        } else {
+            alert('Something went wrong!');
+            return false;
+        }
+    });
+
+    $(document).on('click', 'th .checklist', function() {
+        if ($(this).is(':checked')) {
+            $('td .checklist').prop('checked', true);
+        } else {
+            $('td .checklist').prop('checked', false);
+        }
+    });
+
+    $(document).on('click', 'td .checklist', function() {
+        var lengthChecked = $('td .checklist:checked').length;
+        var lengthCeckbox = $('td .checklist').length;
+
+        if (lengthChecked == lengthCeckbox) {
+            $('th .checklist').prop('checked', true);
+        } else {
+            $('th .checklist').prop('checked', false);
+        }
+    });
+</script>
 @endsection
