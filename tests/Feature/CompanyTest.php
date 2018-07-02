f0840e72e11ad435aa63c64182b7a04ba15c716d
Yoga <yoga.oga94@gmail.com>
2018-07-02 12:56:26 +0700

    Test Unit for Server

--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -6,15 +6,42 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Models\Master\Company;
+use DB;
+use Sentinel;
+
 class CompanyTest extends TestCase
 {
+
+	private $admin;
+	private $company;
+	private $data;
+
+	protected function prepare()
+	{
+		$user = Sentinel::findById(1);
+		$this->admin = Sentinel::login($user);
+		// $company = Company::create([
+		// 	'name' => 'WIT',
+		// 	'address' => 'Bandung',
+		// 	'phone' => '082219230981',
+		// 	'tax' => '2321312',
+		// 	'npwp' => '321343',
+		// ]);
+
+		// $this->company = $company;
+	}
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testIndex()
     {
-        $this->assertTrue(true);
+    	$this->prepare();
+    	$actual = $this->call('GET', 'system/company');
+
+    	$actual->assertStatus(200);
     }
 }
