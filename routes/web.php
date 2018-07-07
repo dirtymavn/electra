d4ab8b63e511db3b6e5f24620ef2659cd7eaa89a
Yoga <yoga.oga94@gmail.com>
2018-07-07 08:38:39 +0700

    Push Route

--- routes/web.php
+++ routes/web.php
@@ -91,6 +91,7 @@ Route::group([ 'middleware' => 'sentinel_auth' ], function () {
         Route::post('inventory/inventory-pkg-detail', array('as' => 'inventory.pkg-detail.post', 'uses' => 'InventoryController@inventoryDetailPkg', 'middleware' => 'sentinel_access:admin.company,inventory.create'));
         Route::post('inventory/inventory-detail/detail', array('as' => 'inventory.detail.detail', 'uses' => 'InventoryController@inventoryDetailGetDetail', 'middleware' => 'sentinel_access:admin.company,inventory.create'));
         Route::post('inventory/inventory-car-detail', array('as' => 'inventory.car-detail.post', 'uses' => 'InventoryController@inventoryDetailCar', 'middleware' => 'sentinel_access:admin.company,inventory.create'));
+        Route::post('inventory/inventory-car-transfer-detail', array('as' => 'inventory.car-transfer-detail.post', 'uses' => 'InventoryController@inventoryCarTransfer', 'middleware' => 'sentinel_access:admin.company,inventory.create'));
         // end Inventory
         // Outbound
         Route::group(['prefix' => 'outbound', 'namespace' => 'Outbound'], function () {
